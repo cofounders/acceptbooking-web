f85917bc72e617657e4f246fd5beca2e2e3cbbd8
Sebastiaan Deckers <seb@ninja.sg>
2013-06-16 20:42:50 +0800

    Deprecating dummy code to fix JSHint

--- source/js/modules/Locations.js
+++ source/js/modules/Locations.js
@@ -13,24 +13,6 @@ define(['jquery', 'underscore', 'backbone', 'app'
 		fetch: function (options) {
 			var that = this;
 			setTimeout(function () {
-				// var streets = [
-				// 	'Kallang Pudding Rd',
-				// 	'Sims Ave',
-				// 	'Sengkang East Way',
-				// 	'Changi Road',
-				// 	'Marina Boulevard',
-				// 	'Jalan Terusan'
-				// ];
-				// var street = _.shuffle(streets)[0];
-				// var number = Math.ceil(Math.random() * 200);
-				// var address = '' + number + ' ' + street;
-				// var response = {
-				// 	address: address
-				// };
-				// that.set(response);
-				// if (options && options.success) {
-				// 	options.success(that, response, options);
-				// }
 			}, 100 + Math.random() * 300);
 		}
 	});
