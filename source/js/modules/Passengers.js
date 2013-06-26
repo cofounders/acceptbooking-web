2d6cbcdccbfb243fb220d6d8817d9edf851b290a
Sebastiaan Deckers <seb@ninja.sg>
2013-06-26 17:19:30 +0700

    Location header as resource_uri response property

--- source/js/modules/Passengers.js
+++ source/js/modules/Passengers.js
@@ -10,6 +10,11 @@ define([
 	Models.Passenger = Backbone.Model.extend({
 		url: function () {
 			return app.api('passenger/');
+		},
+		parse: function (response, options) {
+			return _.defaults(response, {
+				resource_uri: options.xhr.getResponseHeader('Location')
+			});
 		}
 	});
 
