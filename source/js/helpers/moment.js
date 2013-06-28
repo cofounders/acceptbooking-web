c79d05d35e014916472296fb979dd0022467e596
Sebastiaan Deckers <seb@ninja.sg>
2013-06-28 12:32:03 +0700

    MomentJS helper for Handlebars

---
+++ source/js/helpers/moment.js
@@ -0,0 +1,23 @@
+define(['underscore', 'moment'], function (_, moment) {
+
+	return function (timestamp, options) {
+		if (!timestamp) {
+			return new Handlebars.SafeString('');
+		}
+
+		var hash = _.defaults(options.hash, {
+			format: '',
+			calendar: false,
+			fromNow: false
+		});
+
+		var time = moment(timestamp);
+
+		var output = hash.fromNow ? time.fromNow() :
+				hash.calendar ? time.calendar() :
+				time.format(hash.format);
+
+		return new Handlebars.SafeString(output);
+	};
+
+});
